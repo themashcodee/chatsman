edf93dd7f7f396326e417983729c3a67c99c6ecb
Mash Codee <codeemash@gmail.com>
2021-09-10 19:40:50 +0530

    url fixes

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Index() {
         <meta name="description" content="A simple chatting web application" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <h1 className="text-4xl text-red-300">Check Authentication</h1>
+      <h1 className="text-4xl text-red-300">Chatsman</h1>
     </div>
   )
 }
