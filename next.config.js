7c020c71c535db3aff596dc4d09bcc7734b3b343
Mash Codee <codeemash@gmail.com>
2021-09-14 16:01:08 +0530

    little change

--- next.config.js
+++ next.config.js
@@ -4,8 +4,13 @@ module.exports = {
     domains: ['chatsmanapi.herokuapp.com'],
   },
   env: {
+    // PROD
     API_URI_BASE: process.env.API_URI_BASE,
     API_URI_REFRESH_TOKEN: process.env.API_URI_REFRESH_TOKEN,
     API_URI_IMAGE_UPLOAD: process.env.API_URI_IMAGE_UPLOAD
+    // FOR DEV
+    // API_URI_BASE: process.env.API_URI_BASE_TEMP,
+    // API_URI_REFRESH_TOKEN: process.env.API_URI_REFRESH_TOKEN_TEMP,
+    // API_URI_IMAGE_UPLOAD: process.env.API_URI_IMAGE_UPLOAD_TEMP
   }
 }
